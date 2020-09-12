02a39892ed0b108370262d5be934b52327222a54
mino-ri <hojo.origami@gmail.com>
2020-09-13 01:51:18 +0900

    [update] Label, TextBox, ListBox のUIを作成

--- Orimath.ViewPlugins/Themes/ThemeBrushes.cs
+++ Orimath.ViewPlugins/Themes/ThemeBrushes.cs
@@ -12,6 +12,7 @@ namespace Orimath.Themes
         public static ThemeBrush CloseButton { get; }
         public static ThemeBrush Workspace { get; }
         public static ThemeBrush Input { get; }
+        public static ThemeBrush Selector { get; }
         public static ThemeBrush ScrollBar { get; }
 
         static ThemeBrushes()
@@ -20,9 +21,10 @@ namespace Orimath.Themes
             var normal = Solid(40, 121, 80);
             var hover = Solid(53, 161, 107);
             var highlight = Solid(255, 153, 0);
+            var highlightLight = Solid(255, 215, 155);
             var highlightBorder = Solid(168, 100, 0);
-            var disabledBack = Solid(242, 242, 242);
-            var disabledFore = Solid(166, 166, 166);
+            var disabledBackground = Solid(242, 242, 242);
+            var disabledForeground = Solid(166, 166, 166);
             var back = Solid(240, 250, 245);
 
             Control = new ThemeBrush
@@ -30,7 +32,7 @@ namespace Orimath.Themes
                 Normal = new BrushSet(normal, Brushes.White, border),
                 Hovered = new BrushSet(hover, Brushes.White, border),
                 Highlight = new BrushSet(highlight, Brushes.White, highlightBorder),
-                Disabled = new BrushSet(disabledFore, disabledBack, Solid(127, 127, 127)),
+                Disabled = new BrushSet(disabledForeground, disabledBackground, Solid(127, 127, 127)),
             };
 
             CloseButton = new ThemeBrush
@@ -45,16 +47,24 @@ namespace Orimath.Themes
             {
                 Normal = new BrushSet(back, Brushes.Black, normal),
                 Hovered = new BrushSet(Solid(210, 240, 225), Brushes.Black, hover),
-                Highlight = new BrushSet(Solid(255, 215, 155), Brushes.Black, highlightBorder),
-                Disabled = new BrushSet(disabledBack, disabledFore, disabledFore),
+                Highlight = new BrushSet(highlightLight, Brushes.Black, highlightBorder),
+                Disabled = new BrushSet(disabledBackground, disabledForeground, disabledForeground),
             };
 
             Input = new ThemeBrush
             {
-                Normal = new BrushSet(Brushes.White, normal, Brushes.Black),
-                Hovered = new BrushSet(Brushes.White, hover, Brushes.Black),
-                Highlight = new BrushSet(Brushes.White, highlight, Brushes.Black),
-                Disabled = new BrushSet(disabledBack, disabledFore, disabledFore),
+                Normal = new BrushSet(Brushes.White, Brushes.Black, normal),
+                Hovered = new BrushSet(Brushes.White, Brushes.Black, hover),
+                Highlight = new BrushSet(Brushes.White, Brushes.Black, highlight),
+                Disabled = new BrushSet(disabledBackground, disabledForeground, disabledForeground),
+            };
+
+            Selector = new ThemeBrush
+            {
+                Normal = new BrushSet(Brushes.White, Brushes.Black, normal),
+                Hovered = new BrushSet(back, Brushes.Black, hover),
+                Highlight = new BrushSet(highlightLight, Brushes.Black, highlightBorder),
+                Disabled = new BrushSet(disabledBackground, disabledForeground, disabledForeground),
             };
 
             ScrollBar = new ThemeBrush
@@ -62,7 +72,7 @@ namespace Orimath.Themes
                 Normal = new BrushSet(back, Solid(166, 226, 196), null),
                 Hovered = new BrushSet(back, hover, null),
                 Highlight = new BrushSet(back, normal, null),
-                Disabled = new BrushSet(disabledBack, disabledFore, null),
+                Disabled = new BrushSet(disabledBackground, Solid(200, 200, 200), null),
             };
         }
 
